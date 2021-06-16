f8f8868d4b54421915ab67d7c1cd7062b0d403f7
mohitS <mohitsrivastava67@gmail.com>
2021-06-16 18:36:33 +0530

    jwt and refresh tokens

---
+++ src/Context.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+export interface MyContext {
+  res: Response;
+  req: Request;
+}
---
+++ src/auth.ts
@@ -0,0 +1,23 @@
+import { User } from "./entity/User";
+import { sign } from "jsonwebtoken";
+
+export const createAccessToken = (user: User, tLimit: string) => {
+  return sign(
+    {
+      userId: user.id,
+      email: user.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: tLimit }
+  );
+};
+export const createRefreshToken = (user: User, tLimit: string) => {
+  return sign(
+    {
+      userId: user.id,
+      email: user.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET!,
+    { expiresIn: tLimit }
+  );
+};
