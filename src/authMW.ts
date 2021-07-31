903576b815e5ba67395fbade0a80a5dc5c3310f4
msx47 <mohitsrivastava67@gmail.com>
2021-08-01 02:33:27 +0530

    finish up

--- src/authMW.ts
+++ src/authMW.ts
@@ -6,11 +6,12 @@ export const authMW: MiddlewareFn<MyContext> = ({ context }, next) => {
   const authorization = context.req.headers["authorization"];
   try {
     if (!authorization) throw new Error("Access Denied");
+
     const token = authorization.split(" ")[1];
     const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
     context.payload = payload as any;
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
     throw new Error("Access Denied");
   }
   return next();
